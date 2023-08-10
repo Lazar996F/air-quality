3ff3a616bd509693930fe2ce642a15a3ec518a8b
Lazar996F <furtulal96@gmail.com>
2023-08-10 09:20:47 +0200

    implement cards view

--- project/components/Logo.js
+++ project/components/Logo.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { Image, StyleSheet } from 'react-native'
 
 export default function Logo() {
-  return <Image source={require('../assets/etfLogo.png')} style={styles.image} />
+  return <Image source={require('../assets/logo-vector.png')} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
   image: {
-    width: 110,
     height: 110,
     marginBottom: 8,
   },
