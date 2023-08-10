e05302f40a98f0b1c2fd6e971a21bc2a7d2ce47b
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-08-10 16:24:10 +0200

    Modify sent parameters to Dashboard screen

--- project/components/Card.js
+++ project/components/Card.js
@@ -1,23 +1,27 @@
 import * as React from 'react';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
+import { citiesData } from '../dummyData';
 
 const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
-const MyComponent = ({ city, qualityValue, navigation }) => (
-  <Card style={styles.cardWrapper}>
-    <Card.Title title={city} subtitle="Card Subtitle" left={LeftContent} />
-    <Card.Content>
-      <Text variant="titleLarge">{city}</Text>
-      <Text variant="bodyMedium">Card content</Text>
-    </Card.Content>
-    <Card.Actions>
-      <Button onPress={() => navigation.navigate('DashboardScreen')}>
-        Saznaj više
-      </Button>
-    </Card.Actions>
-  </Card>
-);
+const MyComponent = ({ city, qualityValue, navigation }) => {
+  const navigateToDashboard = () => {
+    navigation.navigate('DashboardScreen', { selectedCity: city });
+  };
+  return (
+    <Card style={styles.cardWrapper}>
+      <Card.Title title={city} subtitle="Card Subtitle" left={LeftContent} />
+      <Card.Content>
+        <Text variant="titleLarge">{city}</Text>
+        <Text variant="bodyMedium">Card content</Text>
+      </Card.Content>
+      <Card.Actions>
+        <Button onPress={navigateToDashboard}>Saznaj više</Button>
+      </Card.Actions>
+    </Card>
+  );
+};
 
 export default MyComponent;
 
